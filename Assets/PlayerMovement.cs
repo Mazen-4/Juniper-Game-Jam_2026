8c4f66b5054ae0802b85ee6e82169bea2c9d5d59
Mazen-4 <mazen.ismail@msa.edu.eg>
2026-06-19 22:06:59 +0300

    setting up generic adjustments

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float groundDrag = 5f;
@@ -13,32 +13,34 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private bool isGrounded;
-    private float groundCheckDistance = 0.1f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector2 groundCheckSize = new Vector2(0.9f, 0.1f);
+    [SerializeField] private LayerMask groundLayer;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Get input from WASD or Arrow keys
+        // Get horizontal input only
         moveInput.x = Input.GetAxis("Horizontal");
 
+        // Ground check first (overlap box at feet)
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
+
+        // Jump input
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
-
-        // Ground check (raycast down)
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance);
     }
 
     private void FixedUpdate()
     {
-        // Apply horizontal velocity
+        // Only apply horizontal velocity
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
 
         // Apply drag based on grounded state
@@ -58,4 +60,19 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    private void OnDrawGizmos()
+    {
+        if (groundCheck == null) return;
+
+        if (isGrounded)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+
+        Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
+    }
 }
