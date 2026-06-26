c709a445f6f4cf9292761112607f4f87a0cd173b
Mazen-4 <mazen.ismail@msa.edu.eg>
2026-06-26 06:54:27 +0300

    Combine the menu and adding the UI_bars

---
+++ Assets/scripts/Wheel_follow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel_follow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 5f;
+    [SerializeField] private Vector3 offset = new Vector3(11f, 5.5f, 0f);
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
